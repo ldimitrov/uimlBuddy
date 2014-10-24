42767c3ffb2d6e58adbb43e8c7212cc911a83ad0
Lyuben Dimitrov <ldimitrov070@gmail.com>
2014-10-24 18:44:03 +0200

    added vertical layout fxml + controller and model

--- src/uimlbuddy/model/containers/VerticalLayout.java
+++ src/uimlbuddy/model/containers/VerticalLayout.java
@@ -1,9 +1,41 @@
 package uimlbuddy.model.containers;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import uimlbuddy.model.Part;
+
 /**
+ * Model class for a Vertical Layout
  *
  * @author Lyuben
  */
-public class VerticalLayout {
-    
+public class VerticalLayout extends Part {
+
+    private final StringProperty id;
+
+    /**
+     * Default constructor.
+     */
+    public VerticalLayout() {
+        this(null);
+    }
+
+    /**
+     * Constructor with some initial data.
+     *
+     * @param id
+     */
+    public VerticalLayout(String id) {
+        this.id = new SimpleStringProperty(id);
+    }
+
+    @Override
+    public String getId() {
+        return id.get();
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id.set(id);
+    }
 }
---
+++ src/uimlbuddy/view/VerticalLayoutController.java
@@ -0,0 +1,108 @@
+package uimlbuddy.view;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.controlsfx.dialog.Dialogs;
+import uimlbuddy.model.containers.VerticalLayout;
+import uimlbuddy.util.DocumentWriter;
+
+public class VerticalLayoutController {
+
+    @FXML // fx:id="idField"
+    private TextField idField; // Value injected by FXMLLoader
+
+    private Stage dialogStage;
+    private VerticalLayout vl;
+    private boolean okClicked = false;
+    private DocumentWriter dw;
+
+    public VerticalLayoutController() {
+        dw = new DocumentWriter();
+    }
+    
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize() {
+        assert idField != null : "fx:id=\"idField\" was not injected: check your FXML file 'VerticalLayout.fxml'.";
+    }
+    
+    /**
+     * Sets the stage of this dialog.
+     *
+     * @param dialogStage
+     */
+    public void setDialogStage(Stage dialogStage) {
+        this.dialogStage = dialogStage;
+    }
+    
+    /**
+     * Sets the vertical layout to be edited in the dialog.
+     *
+     * @param vl
+     */
+    public void setVerticalLayout(VerticalLayout vl) {
+        this.vl = vl;
+
+        idField.setText(vl.getId());
+    }
+    
+
+    /**
+     * Returns true if the user clicked OK, false otherwise.
+     *
+     * @return
+     */
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+    
+    /**
+     * Called when the user clicks ok.
+     */
+    @FXML
+    void handleOk() {
+        if (isInputValid()) {
+            vl.setId(idField.getText());
+            
+            DocumentWriter.addPart("VerticalLayout", idField.getText());
+            
+            // Drawing on Canvas
+            DocumentWriter.updateCanvas();
+            okClicked = true;
+            dialogStage.close();
+        }
+    }
+
+    /**
+     * Called when the user clicks cancel.
+     */
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
+
+    /**
+     * Validates the user input in the text fields.
+     *
+     * @return true if the input is valid
+     */
+    private boolean isInputValid() {
+        String errorMessage = "";
+
+        // TODO - make check in future for unique IDs.
+        if (idField.getText() == null || idField.getText().length() == 0) {
+            errorMessage += "ID is required!\n";
+        }
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            // Show the error message
+            Dialogs.create()
+                    .title("Invalid ID Field")
+                    .masthead("Please Enter a valid ID")
+                    .message(errorMessage)
+                    .showError();
+            return false;
+        }
+    }
+}
